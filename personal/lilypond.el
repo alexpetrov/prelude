bec3f6758aafe2477823fc0f1eb9be7b52e9053f
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-08-17 18:42:43 +0400

    Created lilypond configuration file

---
+++ personal/lilypond.el
@@ -0,0 +1,44 @@
+;;; lilypond.el --- Lilypond configuration file
+;; Copyright (c) 2013 Alexander Petrov
+
+;;; Commentary:
+
+;; This file configures Lilypond-mode for editing scores
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(defvar lilypond-dir "/home/alexpetrov/lilypond/usr/share/emacs/site-lisp")
+(message lilypond-dir)
+(add-to-list 'load-path lilypond-dir)
+
+(message (car load-path))
+(autoload 'LilyPond-mode "lilypond-mode")
+(setq auto-mode-alist
+      (cons '("\\.ly$" . LilyPond-mode) auto-mode-alist))
+
+(add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
+
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(provide 'lilypond)

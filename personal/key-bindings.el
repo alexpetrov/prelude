0d16a35cc4aeedd3f2a940aa663a2f2e0e5146cc
root <root@localhost.localdomain>
2013-09-25 13:50:12 +0400

    add basic editing key bindings for russian letters

---
+++ personal/key-bindings.el
@@ -0,0 +1,55 @@
+;;; key-bindings.el --- Personal key-bindings configuration file
+;; Copyright (c) 2013 Alexander Petrov
+
+;;; Commentary:
+
+;; This file configures Lilypond-mode for editing scores
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;; Set basic editing commands to russian letters
+;; Go to the end of buffer
+(global-set-key (kbd "M-ю") 'end-of-buffer)
+
+;; Save-buffer
+(global-set-key (kbd "C-x C-ы") 'save-buffer)
+
+;; Forward-char
+(global-set-key (kbd "C-а") 'forward-char)
+
+;; Backward-char
+(global-set-key (kbd "C-и") 'backward-char)
+
+;; Next-line
+(global-set-key (kbd "C-т") 'next-line)
+
+;; Previous-line
+(global-set-key (kbd "C-з") 'previous-line)
+
+;; Forward-word
+(global-set-key (kbd "M-а") 'forward-word)
+
+;; Backward-word
+(global-set-key (kbd "M-и") 'backward-word)
+
+
+(provide 'key-bindings)
+
+;;; key-bindings.el ends here

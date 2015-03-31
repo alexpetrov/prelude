eb48362296d6087e96ac177eb3408838708c501f
alexpetrov <alexpetrov.rb@gmail.com>
2015-03-31 22:47:43 +0300

    Add robe mode

--- personal/packages.el
+++ personal/packages.el
@@ -1,9 +1,10 @@
 (prelude-require-package 'keymap-utils)
 
 ;; Rails Packages
-(prelude-require-packages '(projectile-rails ruby-refactor))
+(prelude-require-packages '(projectile-rails ruby-refactor robe))
 
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 (add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
+(add-hook 'ruby-mode-hook 'robe-mode)
 
 (provide 'packages)

10231e2b0434f6be344a6e7258b6c3fb214385be
alexpetrov <alexpetrov.rb@gmail.com>
2015-04-01 14:55:30 +0300

    Add chruby and set default ruby 2.1.3

--- personal/packages.el
+++ personal/packages.el
@@ -1,7 +1,9 @@
 (prelude-require-package 'keymap-utils)
 
 ;; Rails Packages
-(prelude-require-packages '(projectile-rails ruby-refactor robe))
+(prelude-require-packages '(chruby projectile-rails ruby-refactor robe))
+(require 'chruby)
+(chruby "2.1.3")
 
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 (add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)

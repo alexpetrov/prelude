7a4286d4357751722996aee48d746f0932df48bf
alexpetrov <alexpetrov.rb@gmail.com>
2015-04-01 15:17:24 +0300

    Create new ruby-config file and add integration between company and robe

--- personal/packages.el
+++ personal/packages.el
@@ -1,12 +1,3 @@
 (prelude-require-package 'keymap-utils)
 
-;; Rails Packages
-(prelude-require-packages '(chruby projectile-rails ruby-refactor robe))
-(require 'chruby)
-(chruby "2.1.3")
-
-(add-hook 'projectile-mode-hook 'projectile-rails-on)
-(add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
-(add-hook 'ruby-mode-hook 'robe-mode)
-
 (provide 'packages)
---
+++ personal/ruby-config.el
@@ -0,0 +1,11 @@
+(prelude-require-packages '(chruby projectile-rails ruby-refactor robe))
+(require 'chruby)
+(chruby "2.1.3")
+
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+(add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+(provide 'ruby-config)
